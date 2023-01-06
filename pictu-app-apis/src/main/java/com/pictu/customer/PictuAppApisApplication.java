94aba7b857c00f4ae7f071e2ea0f1e98cf012892
Anurag Kumar <anuragkr41@gmail.com>
2023-01-06 23:20:25 +0530

    Custom exception working fine. Model mapper sucessfully implemented.

--- pictu-app-apis/src/main/java/com/pictu/customer/PictuAppApisApplication.java
+++ pictu-app-apis/src/main/java/com/pictu/customer/PictuAppApisApplication.java
@@ -1,13 +1,20 @@
 package com.pictu.customer;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PictuAppApisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PictuAppApisApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
